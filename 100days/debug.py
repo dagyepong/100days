a74c7fbc24f690d30a560f957646b3d7bf152d1a
dagyepong <dennisagyepong2@gmail.com>
2022-04-10 23:24:24 +0000

    reproduced an error

--- 100days/debug.py
+++ 100days/debug.py
@@ -1,5 +1,15 @@
+
+# Describe the problem
+
 def my_function():
     for i in range(1, 20):
         if i == 19:
             print("You got it")
 my_function()
+
+# Reproduce the bug
+
+from random import randint
+dice_imgs = ["😎", "😂", "✌️", "👌", "🙈", "😔"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
