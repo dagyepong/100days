77ec82967999a914de60a73edcf255c4bb00bc2e
dagyepong <dennisagyepong2@gmail.com>
2023-11-12 00:36:57 +0000

    edited code

--- 100days/bill.py
+++ 100days/bill.py
@@ -4,5 +4,5 @@ tip = int(input("How much tip would you like to give? 0, 6, 8, or 10? "))
 people = int(input("How many people are to split the bill? "))
 bill_with_tip = tip / 100 * bill + bill
 each_bill = bill_with_tip // people
-print(f"Each one will pay $ {each_bill} ")
+print(f"Each one will pay ${each_bill} ")
 
